cf1d39df4fb96693d1c65d954dd96a077b97eb74
RANDY AGUILAR <rl106845@rlas-macair-20.rlas-116.org>
2018-08-30 11:39:41 -0500

    Initial Commit

---
+++ APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APP
+//
+//  Created by RANDY AGUILAR on 8/30/18.
+//  Copyright © 2018 High School Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
